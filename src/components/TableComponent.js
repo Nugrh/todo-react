3cfa8d9aa62b13b5125a6bc558f6f91801279edd
nugrh <anugrah.tribima@gmail.com>
2022-02-12 23:08:09 +0700

    some updates

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -1,51 +1,49 @@
-import React from 'react';
-import { Table, Space, Row, Col, Button, Divider } from 'antd';
-import 'antd/dist/antd.css';
-import EditModal from './EditModal';
-import { connect } from "react-redux";
-import CreateModal from './CreateModal';
-import { getTodosList } from '../actions/TodoAction';
-
-const columns = [
-    {
-        title: 'Todo',
-        dataIndex: 'todo',
-        key: 'todo',
-        render: text => <a>{text}</a>,
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        fixed: 'right',
-        width: 100,
-        render: (text, record) => (
-            <Space split={<Divider type="vertical" />} size="middle">
-                <a>Done</a>
-                <EditModal />
-                <a>Delete</a>
-            </Space>
-        ),
-    },
-];
-
+import React from "react";
+import { Table, Space, Divider, Popconfirm } from "antd";
+import "antd/dist/antd.css";
+import EditModal from "./EditModal";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteTodo } from "../features/Todos";
 
 const TableComponent = (props) => {
+    const dispatch = useDispatch();
+    const todoList = useSelector((state) => state.todos.value);
+
+    const columns = [
+        {
+            title: "TODO",
+            dataIndex: "todo",
+            key: "todo",
+            render: (text) => <a>{text}</a>,
+        },
+        {
+            title: "ACTION",
+            key: "action",
+            fixed: "right",
+            width: 100,
+            render: (text, record) => (
+                <Space split={<Divider type="vertical" />} size="middle">
+                    <a>Done</a>
+                    <EditModal />
+
+                    <Popconfirm
+                        title="Sure to delete?"
+                        onConfirm={() => {
+                            dispatch(deleteTodo({ id: 2 }));
+                        }}
+                    >
+                        <a>Delete</a>
+                    </Popconfirm>
+                </Space>
+            ),
+        },
+    ];
+
     return (
-        <div>
-
-            {props.getTodosList ?
-                <div>
-                    <Table columns={columns} dataSource={props.getTodosList} />
-                </div>
-                : null}
+        <div style={{ marginTop: 12 }}>
+            <Table columns={columns} dataSource={todoList} rowKey="id" />
         </div>
-    )
-}
-const mapStateToProps = (state) => (
-    {
-        getTodosList: state.todos.getTodosList,
-        errorTodoslist: state.todos.errorTodoslist
-    }
-)
+    );
+};
 
-export default connect(mapStateToProps)(TableComponent)
+export default TableComponent;
