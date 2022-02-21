ff1f074c89f64ce1a13739dec671ac5917225ecc
Anugrah Tribima <73867671+Nugrh@users.noreply.github.com>
2022-02-21 16:27:54 +0700

    Edit Updates

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
-import { Table, Space, Divider, Popconfirm } from "antd";
-import "antd/dist/antd.css";
-import EditModal from "./EditModal";
+import React from "react";
+import { Table, Space, Divider, Popconfirm, message } from "antd";
+import EditModal from "./Modal/EditModal";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteTodo } from "../features/Todos";
+import Checkbox from "antd/lib/checkbox/Checkbox";
+import EditForm from "./Modal/Form/EditForm";
+import "../App.css";
 
-const TableComponent = (props) => {
-    // TODO: pass data id to <EditModal />
+const TableComponent = () => {
 
     const dispatch = useDispatch();
     const todoList = useSelector((state) => state.todos.value);
@@ -16,7 +17,7 @@ const TableComponent = (props) => {
             title: "TODO",
             dataIndex: "todo",
             key: "todo",
-            render: (text) => <a>{text}</a>,
+            render: (text) => <div>{text}</div>,
         },
         {
             title: "ACTION",
@@ -26,29 +27,33 @@ const TableComponent = (props) => {
             width: 100,
             render: (rows, record) => [
                 <Space split={<Divider type="vertical" />} size="middle">
-                    <a>Done</a>
+                    {/* DONE CHECKBOX */}
+                    <Checkbox />
 
                     {/* EDIT DATA MODAL */}
-                    <EditModal id={rows} />
+                    <EditModal todo={record.todo} id={rows} />
+
                     {/* id={id} untuk mengirim data ID untuk parameter function di EditModal component */}
 
                     {/* DELETE DATA */}
                     <Popconfirm // popup konfirmasi yakin hapus data?
-                        title="?"
+                        title="Are you sure to delete this task?"
                         onConfirm={() => {
+                            // manggil function redux deleteTodo di "../features/Todos"
                             dispatch(deleteTodo({ id: rows }));
+                            message.success('Task deleted');
                         }}
                     >
                         <a>Delete</a>
                     </Popconfirm>
-                </Space>,
+                </Space>
             ],
         },
     ];
 
     return (
         <div style={{ marginTop: 12 }}>
-            <Table columns={columns} dataSource={todoList} rowKey="id" />
+            <Table columns={columns} dataSource={todoList} pagination={{ defaultPageSize: 5 }} rowKey="id" />
         </div>
     );
 };
