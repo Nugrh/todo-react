a0706c19968d95a62505d1431148249c70b8e0c1
Anugrah Tribima <73867671+Nugrh@users.noreply.github.com>
2022-03-04 16:49:58 +0700

    Some Updates

--- src/components/MyTodo/Modal/Form/CreateForm.js
+++ src/components/MyTodo/Modal/Form/CreateForm.js
@@ -1,51 +1,51 @@
-import React  from "react";
+import React from "react";
 import { Modal, Input, Form, message } from "antd";
 
 const CreateForm = ({ visible, onCreate, onCancel }) => {
-    const [form] = Form.useForm();
+  const [form] = Form.useForm();
 
-    return (
-        <Modal
-            visible={visible}
-            title="Create"
-            okText="Create"
-            cancelText="Cancel"
-            onOk={() => {
-                form
-                    .validateFields()
-                    .then((values) => {
-                        form.resetFields();
-                        onCreate(values);
-                        message.success('Task added');
-                    })
-                    .catch((info) => {
-                        console.log('Validate Failed:', info);
-                    });
-            }}
-            onCancel={onCancel}
+  return (
+    <Modal
+      visible={visible}
+      title="Create"
+      okText="Create"
+      cancelText="Cancel"
+      onOk={() => {
+        form
+          .validateFields()
+          .then((values) => {
+            form.resetFields();
+            onCreate(values);
+            message.success("Task added");
+          })
+          .catch((info) => {
+            console.log("Validate Failed:", info);
+          });
+      }}
+      onCancel={onCancel}
+    >
+      <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
+        initialValues={{
+          modifier: "public",
+        }}
+      >
+        <Form.Item
+          name="task"
+          rules={[
+            {
+              required: true,
+              message: "Task cannot be empty!",
+            },
+          ]}
         >
-            <Form
-                form={form}
-                layout="vertical"
-                name="form_in_modal"
-                initialValues={{
-                    modifier: 'public',
-                }}
-            >
-                <Form.Item
-                    name="task"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Task cannot be empty!',
-                        },
-                    ]}
-                >
-                    <Input placeholder="Input todo..." />
-                </Form.Item>
-            </Form>
-        </Modal>
-    );
+          <Input placeholder="Input todo..." autoFocus />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
 };
 
 export default CreateForm;
--- src/components/MyTodo/Modal/Form/EditForm.js
+++ src/components/MyTodo/Modal/Form/EditForm.js
@@ -1,57 +1,50 @@
 import React from "react";
 import { Modal, Input, Form, message } from "antd";
 
-const EditForm = ({ visible, onUpdate, onCancel, todo }) => {
-    const [form] = Form.useForm();
-    return (
-        <Modal
-            visible={visible}
-            title="Edit"
-            okText="Edit"
-            cancelText="Cancel"
-            onOk={() => {
-                form
-                    .validateFields()
-                    .then((values) => {
-                        onUpdate(values);
-                        message.success('Task edited');
-                    })
-                    .catch((info) => {
-                        console.log('Validate Failed:', info);
-                    });
-            }}
-            onCancel={
-                (form.resetFields(),
-                onCancel)
-            }
+const EditForm = ({ visible, onUpdate, onCancel, task }) => {
+  const [form] = Form.useForm();
+  return (
+    <Modal
+      visible={visible}
+      title="Edit"
+      okText="Edit"
+      cancelText="Cancel"
+      onOk={() => {
+        form
+          .validateFields()
+          .then((values) => {
+            onUpdate(values);
+            message.success("Task edited");
+          })
+          .catch((info) => {
+            console.log("Validate Failed:", info);
+          });
+      }}
+      onCancel={(form.resetFields(), onCancel)}
+    >
+      <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
+        initialValues={{
+          modifier: "public",
+        }}
+        autoComplete="off"
+      >
+        <Form.Item
+          name="newTask"
+          rules={[
+            {
+              required: true,
+              message: "Task cannot empty!",
+            },
+          ]}
         >
-            <Form
-                form={form}
-                layout="vertical"
-                name="form_in_modal"
-                initialValues={{
-                    modifier: 'public',
-                }}
-                autoComplete="off"
-
-            >
-                <Form.Item
-                    name="newTask"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Todo cannot empty!',
-                        },
-                    ]}
-                >
-                    <Input
-                        placeholder="Input todo..."
-                        defaultValue={todo}
-                    />
-                </Form.Item>
-            </Form>
-        </Modal >
-    );
+          <Input placeholder="Input todo..." defaultValue={task} autoFocus />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
 };
 
 export default EditForm;
